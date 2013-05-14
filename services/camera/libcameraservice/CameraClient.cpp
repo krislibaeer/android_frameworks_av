010ec44018ec2e4d33bdb9ce7f7a4176991a45e8
krislibaeer <krislibaeer@googlemail.com>
2013-05-14 16:13:45 +0200

    correct the rotation of front cam

--- services/camera/libcameraservice/CameraClient.cpp
+++ services/camera/libcameraservice/CameraClient.cpp
@@ -1020,13 +1020,13 @@ int CameraClient::getOrientation(int degrees, bool mirror) {
         else if (degrees == 180) return HAL_TRANSFORM_ROT_180;
         else if (degrees == 270) return HAL_TRANSFORM_ROT_270;
     } else {  // Do mirror (horizontal flip)
-        if (degrees == 0) {           // FLIP_H and ROT_0
+        if (degrees == 180) {           // FLIP_H and ROT_0
             return HAL_TRANSFORM_FLIP_H;
-        } else if (degrees == 90) {   // FLIP_H and ROT_90
+        } else if (degrees == 270) {   // FLIP_H and ROT_90
             return HAL_TRANSFORM_FLIP_H | HAL_TRANSFORM_ROT_90;
-        } else if (degrees == 180) {  // FLIP_H and ROT_180
+        } else if (degrees == 0) {  // FLIP_H and ROT_180
             return HAL_TRANSFORM_FLIP_V;
-        } else if (degrees == 270) {  // FLIP_H and ROT_270
+        } else if (degrees == 90) {  // FLIP_H and ROT_270
             return HAL_TRANSFORM_FLIP_V | HAL_TRANSFORM_ROT_90;
         }
     }
